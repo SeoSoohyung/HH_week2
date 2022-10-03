82ed69bfa723e3c28e4b5119a24b12957beb4ffe
SeoSoohyung <spartaseosu@gmail.com>
2022-10-03 14:08:50 +0900

    수정하였다

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
     mongoose
-    .connect("mongodb://admin:1234@3.34.127.89:27017/admin",
+    .connect("mongodb://admin:1234@3.34.127.89:27017/admin?authSource=admin",
     {
         useUnifiedTopology: true,
         useNewUrlParser: true,
