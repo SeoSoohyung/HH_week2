d6ed293c50ba0bec581d21cdfa5ae8754fbd0979
SeoSoohyung <spartaseosu@gmail.com>
2022-10-03 14:01:05 +0900

    수정완료

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
     mongoose
-    .connect("mongodb://localhost:27017/admin")
+    .connect("mongodb://3.34.127.89:27017/admin")
     .catch(err => console.log(err));
 };
 
