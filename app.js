9a4beeedac98c56c71580d09effcc178a3afbf0a
SeoSoohyung <spartaseosu@gmail.com>
2022-10-03 10:06:55 +0900

    주석추가

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express"); // express 값 가져온다.
 const postrouter = require("./routes/posts") 
 const commtrouter = require("./routes/comments")
 const app = express();
-const connect = require("./schemas");
+const connect = require("./schemas"); //DB에 연결해주는거
 connect();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/post',[postrouter]);
+app.use('/post',[postrouter]); 
 app.use('/comment',[commtrouter]);
 
 app.listen(3000, () => {
